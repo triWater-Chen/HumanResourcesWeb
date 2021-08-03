eb3a8efc9e3f57ce10f774a73b4d2455637a204a
Chen <49113016+triWater-Chen@users.noreply.github.com>
2021-08-03 22:40:28 +0800

    实现职称管理删改功能（未实现批量删除）

--- src/components/page/system/JobLevel.vue
+++ src/components/page/system/JobLevel.vue
@@ -81,6 +81,18 @@
                           header-align="center"
                           align="center"
         />
+        <el-table-column label="是否启用"
+                         header-align="center"
+                         align="center"
+        >
+          <template slot-scope="scope">
+<!--            <el-tag type="success" v-if="scope.row.enabled">已启用</el-tag>-->
+<!--            <el-tag type="danger" v-else>未启用</el-tag>-->
+            <el-switch v-model="scope.row.enabled"
+                       @change="handleStatus(scope.row)"
+            />
+          </template>
+        </el-table-column>
         <el-table-column label="操作"
                          header-align="center"
                          align="center"
@@ -105,6 +117,44 @@
         </el-table-column>
       </el-table>
     </div>
+    <el-dialog title="修改职称"
+               :visible.sync="dialogVisible"
+               width="420px"
+               style="text-align: center"
+    >
+      <el-form>
+        <el-form-item>
+          <el-tag>职位名称</el-tag>
+          <el-input v-model="editPost.name"
+                    size="medium"
+                    style="width: 200px; margin-left: 10px;"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-tag>职称级别</el-tag>
+          <el-select v-model="editPost.titleLevel"
+                     size="medium"
+                     style="width: 200px; margin-left: 10px;"
+          >
+            <el-option v-for="item in titleLevels"
+                       :key="item"
+                       :label="item"
+                       :value="item"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-switch v-model="editPost.enabled"
+                     active-text="启用"
+                     inactive-text="禁用"
+          />
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button size="small" @click="dialogVisible = false">取 消</el-button>
+        <el-button size="small" type="primary" @click="handleEdit">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -120,7 +170,9 @@ export default {
         '初级',
         '员级',
       ],
+
       multipleSelection: [],
+      ids: [],
 
       // 用于添加职称
       addLevel: {
@@ -128,6 +180,13 @@ export default {
         titleLevel: '',
       },
       levels: [],
+
+      dialogVisible: false,
+      editPost : {
+        name: '',
+        titleLevel: '',
+        enabled: '',
+      },
     }
   },
 
@@ -154,6 +213,11 @@ export default {
             this.$message.success(res.message)
             this.initJobLevel()
           }
+          // 将添加值清空
+          this.addLevel = {
+            name: '',
+            titleLevel: '',
+          }
         })
       } else {
         if (!this.addLevel.name) {
@@ -162,16 +226,63 @@ export default {
           this.$message.error("职称等级不可为空")
         }
       }
-      this.addLevel = {
-        name: '',
-        titleLevel: '',
+    },
+
+    // ----- 修改状态 -----
+    handleStatus(data) {
+      const text = data.enabled === true ? "启用" : "停用"
+      this.$confirm('确定要"' + text + '"职称【' + data.name + '】吗?',
+          '提示',
+          {
+            confirmButtonText: '确 定',
+            cancelButtonText: '取 消',
+            type: 'warning'
+          }).then(() => {
+            this.API.jobLevelUpdate(data).then(res => {
+              if (res.success) {
+                this.$message.success(text + "成功")
+              }
+            })
+          }).catch(() => {
+            data.enabled = data.enabled !== true
+      })
+    },
+
+    // ----- 编辑职称 -----
+    showEdit(data) {
+      Object.assign(this.editPost, data)
+      this.dialogVisible = true
+    },
+    handleEdit() {
+      if (this.editPost.name) {
+        this.API.jobLevelUpdate(this.editPost).then(res => {
+          if (res.success) {
+            this.$message.success(res.message)
+            this.initJobLevel()
+            this.dialogVisible = false
+          }
+        })
       }
     },
 
-    showEdit() {
-    },
-
-    handleDelete() {
+    // ----- 删除职称 -----
+    handleDelete(data) {
+      this.$confirm('此操作将永久删除职称【' + data.name + '】, 是否继续?',
+          '提示',
+          {
+            confirmButtonText: '确 定',
+            cancelButtonText: '取 消',
+            type: 'warning'
+          }).then(() => {
+            const deleteId = []
+            deleteId.push(data.id)
+            this.API.jobLevelRemoveBatch(deleteId).then(res => {
+              if (res.success) {
+                this.$message.success(res.message)
+                this.initJobLevel()
+              }
+            })
+          }).catch(() => {})
     },
     handleSelectionChange() {
     },
