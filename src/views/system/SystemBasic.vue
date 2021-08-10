441b83c3dd63b7c3cae549b8fae45866b838c720
Chen <49113016+triWater-Chen@users.noreply.github.com>
2021-08-10 15:15:47 +0800

    标签页改为懒加载

--- src/views/system/SystemBasic.vue
+++ src/views/system/SystemBasic.vue
@@ -3,16 +3,16 @@
     <el-tab-pane label="部门管理" name="department">
       <Department />
     </el-tab-pane>
-    <el-tab-pane label="职位管理" name="position">
+    <el-tab-pane label="职位管理" name="position" :lazy="true">
       <Position />
     </el-tab-pane>
-    <el-tab-pane label="职称管理" name="jobLevel">
+    <el-tab-pane label="职称管理" name="jobLevel" :lazy="true">
       <JobLevel />
     </el-tab-pane>
-    <el-tab-pane label="奖惩规则" name="rewardPenalty">
+    <el-tab-pane label="奖惩规则" name="rewardPenalty" :lazy="true">
       <RewardPenalty />
     </el-tab-pane>
-    <el-tab-pane label="权限管理" name="permission">
+    <el-tab-pane label="权限管理" name="permission" :lazy="true">
       <Permission />
     </el-tab-pane>
   </el-tabs>
